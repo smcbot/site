1e910cfd8049329d7d9eeb132495a5ac9c1b922b
smcbot <75273880+smcbot@users.noreply.github.com>
2022-12-26 19:51:10 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/renderer.js",
+      "/vex-theme-plain.css",
+      "/vex.combined.min.js",
+      "/vex.css",
+      "/icon.png",
+    ])
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+})
